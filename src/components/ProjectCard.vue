d1f455dc43e2a157ccf6f6226c09977d61b62c20
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-09-06 22:32:54 +0200

    store importato

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,46 @@
+<script>
+import {store} from '../store.js';
+
+    export default {
+            name:"ProjectCard",
+            props:{
+                project:Object
+            },
+            data() {
+                return {
+                    store,
+                    maxNumCharacters:200,
+                }
+            },
+            methods: {
+                truncateText(text){
+                    if(text.length>50){
+                    return text.substr(0,50 ) + '...';
+                     }
+                return text;
+                }
+            }
+        }
+</script>
+<template lang="">
+    
+    <div class="card my-3 min-height-250px">
+        <div class="card-header">
+            <h2 class="text-center">{{project.title}}</h2>
+        </div>
+        <div class="card-body m-1">
+            <span v-if="project.type">
+                {{project.type.name}}
+            </span>
+            <span v-else="project.type" >Tipologia non asegnata</span>
+             <p>{{truncateText(project.description)}}</p> 
+        </div>
+            <div class="card-footer">
+                <a href="#" class="btn btn-sm btn-primary">leggi piu' informazioni</a>
+            </div>
+         </div>
+    
+</template>
+<style lang="">
+    
+</style>
--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,18 +1,23 @@
 <script>
 import axios from 'axios';
+
+import {store} from '../store.js';
+
 import AppLoader from '../components/AppLoader.vue';
+import ProjectCard from '../components/ProjectCard.vue';
 
 export default {
     name: 'ProjectList',
     components:{
-        AppLoader
+        AppLoader,
+        ProjectCard
     },
     data(){
         return{
-            baseUrl: 'http://localhost:8000',
+
+            store,
             projects:[],
             loading: true,
-            maxNumCharacters:200,
             currentPage:1,
             lastPage:null,
         
@@ -35,19 +40,14 @@ export default {
 
             //     }
             // })
-            axios.get(`${this.baseUrl}/api/projects`,{params: {page: num_page}}).then((response)=>{
+            axios.get(`${this.store.baseUrl}/api/projects`,{params: {page: num_page}}).then((response)=>{
                 this.projects = response.data.results.data;
                 this.currentPage = response.data.results.current_page;
                 this.lastPage = response.data.results.last_page;
                 this.loading =false;
             })
         },
-        truncateText(text){
-            if(text.length>50){
-                return text.substr(0,50 ) + '...';
-            }
-            return text;
-        }
+        
     }
 }
 </script>
@@ -63,41 +63,27 @@ export default {
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-4" v-for="project in projects" :key="project.id" >
-                <div class="card my-3 min-height-250px">
-                    <div class="card-header">
-                        <h2 class="text-center">{{project.title}}</h2>
-                    </div>
-                    <div class="card-body m-1">
-                        <span v-if="project.type">
-                                {{project.type.name}}
-                        </span>
-                        <span v-else="project.type" >Tipologia non asegnata</span>
-                       <p>{{truncateText(project.description)}}</p> 
-                    </div>
-                    <div class="card-footer">
-                        <a href="#" class="btn btn-sm btn-primary">leggi piu' informazioni</a>
-                    </div>
-               </div>
-                
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <div class="d-flex justify-content-center">
-                        <nav>
-                            <ul class="pagination">
+                <ProjectCard :project="project"/>
+            </div>             
+         </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-center">
+                    <nav>
+                        <ul class="pagination">
                                 <li :class="currentPage === 1 ? 'disabled' : ''">
                                     <button class="page-link"  @click="getProjects(currentPage -1)" >precedente</button>
                                 </li>
                                 <li :class="currentPage === lastPage ? 'disabled' : ''">
                                     <button class="page-link" @click="getProjects(currentPage+1)">successivo</button>
                                 </li>
-                            </ul>
-                        </nav>
-                    </div>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
-    </div>
+     </div>
+    
 </template>
 <style lang="scss">
     .min-height-250px{
